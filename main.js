f25654e197efdeeb5c1d245a9c7d73df9eb097bc
Giacomo Titti <giacomotitti@gmail.com>
2022-03-28 23:02:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -38,29 +38,6 @@ properties: ['gridcoll_classifier'],
 reducer: ee.Reducer.first()
 });
 
-//-------------------------------------------------
-//spatial cross-validation
-var SCV = require('users/gabrielenicolanapoli/Gully:SCV');
-var CV = SCV.SCV(gridcoll,['S_mean', 'S_std','Prec_mean','Prec_std','NDVI_mean','NDVI_std'
-  ,'NDWI_mean','NDWI_std','HCv_mean','HCv_std','VCv_mean','VCv_std'],2500)
-//var suscValid = ee.FeatureCollection(suscValidC.flatten())
-var gridScv=CV['cov']
-var suscValidC=CV['susc']
-
-var suscValid = ee.FeatureCollection(suscValidC.flatten())
-
-
-//------------------------------------------
-/*var multiROC = require('users/gabrielenicolanapoli/Gully:multiROC');
-var susc=ee.FeatureCollection(suscValidC)
-var tt = multiROC.quality('gridcoll_classifier',susc,'lsd_bool')*/
-
-//---------------------------------------------
-//ROC of SCV
-var suscValidImage = suscValid.reduceToImage({
-properties: ['gridcoll_classifier'],
-reducer: ee.Reducer.first()
-});
 
 //------------------------------------------------
 
@@ -76,14 +53,8 @@ var numbers=function(layer){
   var AUC=ROCobject['AUC']
   //print(AUC)
 
-  //print(ee.Number(ROC_best.get('ROC_best')))
-  var truefalse = ROC.binary('gridcoll_classifier',suscValid,'lsd_bool', ROC_best);
-
-  var tptf = truefalse.reduceToImage({
-  properties: ['tptf'],
-  reducer: ee.Reducer.first()
   });
-  return {'chartROC':chartROC,'ROC_best':ROC_best,'AUC':AUC,'tptf':tptf}
+  return {'chartROC':chartROC,'ROC_best':ROC_best,'AUC':AUC}
   return ROCobject
 }
 
@@ -93,27 +64,17 @@ var numbers=function(layer){
 
 //-------------------------------------------------
 var show=function(){
-var DY = require('users/gabrielenicolanapoli/Gully:display_5');
+var DY = require('users/gabrielenicolanapoli/Gully:display');
 
 var images={'Calibration map':suscFitImage,
-  'Validation map':suscValidImage,
-  //'TrueFalse': tptf,
   'Prediction map': suscPredImage
 }
-
-//Map.addLayer(images['SIfit'])
-//print(truefalse)
-//print(truefalse.filter(ee.Filter.eq('tptf',0)))
-
-//var splitPanel=DY.splitt(images,trainAccuracy,chartROC,ROC_best,AUC)
-var splitPanel=view(images,trainAccuracy,gridScv,suscValid,suscFit)
-
-//ui.root.widgets().reset([splitPanel]);
+var splitPanel=view(images,trainAccuracy,suscFit)
 }
 
 
 //--------------------------------------------
-var view=function(images,trainAccuracy,gridScv,suscValid,suscFit){
+var view=function(images,trainAccuracy,suscFit){
   var DY = require('users/gabrielenicolanapoli/Gully:display_5');
   var paletteone = ['d10e00ff', 'df564dff', 'eb958fff', 'f0b2aeff', 'ffffffff',
     'dbeaddff', '4e9956ff', '1b7b25ff', '006b0bff']
@@ -131,29 +92,22 @@ var view=function(images,trainAccuracy,gridScv,suscValid,suscFit){
 
   var rightMap = ui.Map();
 
-  var button1 = ui.Button({style:{position: 'bottom-right'},label: 'Run Validation  ROC-analysis',onClick: function() {
-    var num=numbers(V);
-    rightMap.add(DY.createnumbers(trainAccuracy,num['chartROC'],num['ROC_best'],num['AUC']));
-    rightMap.addLayer(num['tptf'],colorizedVis1,'Confusion map')
-  }});
-
   var button2 = ui.Button({style:{position: 'bottom-right'},label: 'Run Calibration ROC-analysis',onClick: function() {
-    var num=numbers(T);
+    var num=numbers(suscFit);
     rightMap.add(DY.createnumbers(trainAccuracy,num['chartROC'],num['ROC_best'],num['AUC']));
     //rightMap.addLayer(num['tptf'],colorizedVis1,'Confusion map')
   }});
 
-  rightMap.add(button1)
   rightMap.add(button2)
 
   //rightMap.add(createview())
   rightMap.add(DY.createLegend(colorizedVis,colorizedVis1))
   rightMap.setControlVisibility(true);
   rightMap.addLayer(images['Calibration map'],colorizedVis,'Calibration map')
-  rightMap.addLayer(images['Validation map'],colorizedVis,'Validation map')
+  
   //rightMap.addLayer(images['TrueFalse'],colorizedVis1,'TrueFalse')
   rightMap.addLayer(images['Prediction map'],colorizedVis,'Prediciton map')
-  rightMap.addLayer(gridScv,{},'Spatial CV grid')
+  
 
   var linker = ui.Map.Linker([rightMap,leftMap]);
 
