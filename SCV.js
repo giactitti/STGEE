57e813a00c5fd6d496081ae0bf2d6bf93522a547
Giacomo Titti <giacomotitti@gmail.com>
2022-04-20 08:22:18 +0200

    Update SCV.js

--- SCV.js
+++ SCV.js
@@ -1,3 +1,31 @@
+/*
+***************************************************************************
+    STGEE
+        begin                : 2022-04
+        copyright            : (C) 2022 by Giacomo Titti and Gabriele Nicola Napoli,
+                               Bologna, April 2022
+        email                : giacomotitti@gmail.com
+***************************************************************************
+
+***************************************************************************
+    STGEE
+    Copyright (C) 2022 by Giacomo Titti and Gabriele Nicola Napoli, Bologna, April 2022
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ***************************************************************************
+*/
+
 //-------------------
 
 //Training the classifer and applying it with the filtered training collection.
